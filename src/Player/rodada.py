fc7de239749c1c3eabc70aaef8a916c8c04157cd
daviludvig <ludviglongen@gmail.com>
2023-10-17 09:56:07 -0300

    Parcialmente finalizadas classes de deck

--- src/Player/rodada.py
+++ src/Player/rodada.py
@@ -2,5 +2,11 @@ from src.Deck.baralho import Baralho
 
 class Rodada():
     def __init__(self, baralho, quantidade):
+       self.setCartasRodada(baralho, quantidade) 
+
+    def setCartasRodada(self, baralho, quantidade):
         self.cartasRodada = baralho.distribuir_cartas(quantidade)
+
+    def getCartasRodada(self):
+        return self.cartasRodada
         
