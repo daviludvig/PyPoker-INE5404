cdc66e77772733643d9d8c9f57afabe86916d98e
daviludvig <ludviglongen@gmail.com>
2023-10-17 09:47:16 -0300

    Started classes and organizations

---
+++ src/Deck/baralho.py
@@ -0,0 +1,6 @@
+class Baralho():
+    def __init__(self):
+        naipes = ["Paus", "Copas", "Espadas", "Ouro"]
+        valores = ["Ás", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei"]
+
+        self.baralho = []
