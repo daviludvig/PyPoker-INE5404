6179001dc5cd0c801e1accf290b662a277655af9
daviludvig <ludviglongen@gmail.com>
2023-10-30 22:26:19 -0300

    Generic commit

--- src/pontuacao.py
+++ src/pontuacao.py
@@ -21,127 +21,90 @@ class Pontuacao():
         self.dois_pares()
         self.um_par()
 
+    def verificar_combinacao(self, valores, quantidades):
+        for valor in valores:
+            if valores.count(valor) >= quantidades:
+                self.score = quantidades
+                return True
+        return False
+
     def royal_flush(self):
         # Método que verifica se o jogador tem um royal flush
-        naipes = []
-        for carta in self.cartas:
-            naipes.append(carta.get_naipe())
-        aux = ["Paus", "Copas", "Espadas", "Ouro"]
-        for naipe in aux:
-            if naipes.count(naipe) >= 5:
-                valores = []
-                for carta in self.cartas:
-                    if carta.get_naipe() == naipe:
-                        valores.append(carta.get_valor())
-                if "Ás" in valores and "Rei" in valores and "Dama" in valores and "Valete" in valores and "10" in valores:
-                    if not self.flag:
-                        self.score = 10
-                        self.flag = True
+        naipes = set([carta.get_naipe() for carta in self.cartas])
+        if len(naipes) < 5:
+            return
+        valores = set([carta.get_valor() for carta in self.cartas])
+        if "Ás" in valores and "Rei" in valores and "Dama" in valores and "Valete" in valores and "10" in valores:
+            self.score = 10
+            self.flag = True
+
     
     def straigth_flush(self):
         # Método que verifica se o jogador tem um straigth flush
-        naipes = []
-        for carta in self.cartas:
-            naipes.append(carta.get_naipe())
-        aux = ["Paus", "Copas", "Espadas", "Ouro"]
-        for naipe in aux:
-            if naipes.count(naipe) >= 5:
-                valores = []
-                for carta in self.cartas:
-                    if carta.get_naipe() == naipe:
-                        valores.append(carta.get_valor())
-                valores.sort()
-                for i in range(len(valores)-4):
-                    if sum(valores)/len(valores) == int(valores[ceil(len(valores)/2)]):
-                        if not self.flag:
-                            self.score = 9
-                            self.flag = True
+        naipes = set([carta.get_naipe() for carta in self.cartas])
+        if len(naipes) < 5:
+            return False
+        valores = set([carta.get_valor() for carta in self.cartas])
+        valores = sorted(valores, key=lambda x: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei", "Ás"].index(x))
+        for i in range(len(valores) - 4):
+            if valores[i:i+5] == valores[i] + 5:
+                if not self.flag:
+                    self.score = 9
+                    self.flag = True
         
     def quadra(self):
         # Método que verifica se o jogador tem uma quadra
-        valores = []
-        for carta in self.cartas:
-            valores.append(carta.get_valor())
-        for valor in valores:
-            if valores.count(valor) >= 4:
-                self.score = 8
-                self.flag = True
+        if not self.flag:
+            self.score = 8
+            self.flag = self.verificar_combinacao([carta.get_valor() for carta in self.cartas], 4)
             
     def full_house(self):
         # Método que verifica se o jogador tem um full house
-        valores = []
-        for carta in self.cartas:
-            valores.append(carta.get_valor())
-        for valor in valores:
-            if valores.count(valor) >= 3:
-                for valor2 in valores:
-                    if valores.count(valor2) >= 2 and valor != valor2:
-                        if not self.flag:
-                            self.score = 7
-                            self.flag = True
+        valores = [carta.get_valor() for carta in self.cartas]
+        if self.verificar_combinacao(valores, 3):
+            if self.verificar_combinacao(valores, 2):
+                if not self.flag:
+                    self.score = 7
+                    self.flag = True
             
     def flush(self):
         # Método que verifica se o jogador tem um flush
-        naipes = []
-        for carta in self.cartas:
-            naipes.append(carta.get_naipe())
-        aux = ["Paus", "Copas", "Espadas", "Ouro"]
-        for naipe in aux:
-            if naipes.count(naipe) >= 5:
-                if not self.flag:
-                    self.score = 6
-                    self.flag = True
+        naipes = [carta.get_naipe() for carta in self.cartas]
+        if not self.flag:
+            self.flag = self.verificar_combinacao(naipes, 5)
+            self.score = 6
             
     def straigth(self):
         # Método que verifica se o jogador tem um straigth
-        valores = []
-        for carta in self.cartas:
-            valores.append(carta.get_valor())
-        valores.sort()
-        for i in range(len(valores)-4):
-            if valores[i] == valores[i+1] - 1 and valores[i+1] == valores[i+2] - 1 and valores[i+2] == valores[i+3] - 1 and valores[i+3] == valores[i+4] - 1:
+        valores = set([carta.get_valor() for carta in self.cartas])
+        valores = sorted(valores, key=lambda x: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Valete", "Dama", "Rei", "Ás"].index(x))
+        for i in range(len(valores) - 4):
+            if valores[i:i+5] == valores[i] + 5:
                 if not self.flag:
                     self.score = 5
                     self.flag = True
 
+
     def trinca(self):
         # Método que verifica se o jogador tem uma trinca
-        valores = []
-        for carta in self.cartas:
-            valores.append(carta.get_valor())
-        for valor in valores:
-            if valores.count(valor) >= 3:
-                if not self.flag:
-                    self.score = 4
-                    self.flag = True
-                    break
+        if not self.flag:
+            self.score = 4
+            self.flag = self.verificar_combinacao([carta.get_valor() for carta in self.cartas], 3)
             
     def dois_pares(self):
         # Metodo que verifica se o jogador tem dois pares
-        valores = []
-        for carta in self.cartas:
-            valores.append(carta.get_valor())
-        for valor in valores:
-            if valores.count(valor) >= 2:
-                for valor2 in valores:
-                    if valores.count(valor2) >= 2 and valor != valor2:
-                        if not self.flag:
-                            self.score = 3
-                            self.flag = True
-                            break
-                break
+        if not self.flag:
+            valores = [carta.get_valor() for carta in self.cartas]
+            if self.verificar_combinacao(valores, 2):
+                if self.verificar_combinacao(valores, 2):
+                    self.score = 3
+                    self.flag = True
     
     def um_par(self):
         # Método que verifica se o jogador tem um par
-        valores = []
-        for carta in self.cartas:
-            valores.append(carta.get_valor())
-        for valor in valores:
-            if valores.count(valor) >= 2:
-                if not self.flag:
-                    self.score = 2
-                    self.flag = True
-                    break
+        if not self.flag:
+            self.flag = self.verificar_combinacao([carta.get_valor() for carta in self.cartas], 2)
+            self.score = 2
     
 
     # Nunca vai chegar nesse método
